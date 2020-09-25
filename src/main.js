01d67561446eabca19a6c95c8baf2d07d2c8a472
liangyouwen <layouwen@gmail.com>
2020-09-26 00:44:03 +0800

    add fastClick

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
+
 import 'normalize.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,

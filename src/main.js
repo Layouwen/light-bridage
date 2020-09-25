a1008852ab1f8724a84512d763ae034c669cf1b4
liangyouwen <layouwen@gmail.com>
2020-09-26 01:51:26 +0800

    add iconfont.css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import fastClick from 'fastclick'
 
 import '../src/assets/styles/reset.css'
 import '../src/assets/styles/border.css'
+import '../src/assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)

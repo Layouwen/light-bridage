6d86f42f24c7e36b6d87742229c11a05a4a7a07c
liangyouwen <layouwen@gmail.com>
2020-09-26 23:16:30 +0800

    测试请求mock是否成功

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,9 +14,21 @@ import HomeSwiper from 'components/Swiper'
 import HomeIcons from 'components/Icons'
 import HomeRecommend from 'components/Recommend'
 import HomeWeekend from 'components/Weekend'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: { HomeWeekend, HomeRecommend, HomeHeader, HomeSwiper, HomeIcons }
+  components: { HomeWeekend, HomeRecommend, HomeHeader, HomeSwiper, HomeIcons },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
 }
 </script>

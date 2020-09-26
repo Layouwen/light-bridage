5d6d7039c310f5772f3c55dc7e6e4453328d1f0d
liangyouwen <layouwen@gmail.com>
2020-09-26 23:10:22 +0800

    配置config设置API Mock

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,22 @@ function resolve (dir) {
 
 module.exports = {
   lintOnSave: true,
+  // 设置别名
   chainWebpack: (config) => {
     config.resolve.alias
       .set('assets', resolve('src/assets'))
       .set('styles', resolve('src/assets/styles'))
       .set('components', resolve('src/components'))
+  },
+  // 添加API Mock重定向
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': '/public/mock'
+        }
+      }
+    }
   }
 }

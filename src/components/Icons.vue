b46c556ecfc200c9beb50466afdffc265613a526
liangyouwen <layouwen@gmail.com>
2020-09-26 21:49:59 +0800

    实现自动分页的swiperIcons栏

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,57 +1,129 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" alt
-             src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png"/>
-      </div>
-      <p class="icon-desc">我是旅游</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" alt
+                 :src="item.imgUrl"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/69e8b07cb2d438c5530aebd4c8e3abd3.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/b7a5e6b1774eda4fc9e32588f1d00c94.png',
+        desc: '城市观光'
+      }, {
+        id: '0004',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/5ffcd4a916f764f678263b27c1e2e00c.png',
+        desc: '踏青赏花'
+      }, {
+        id: '0005',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+        desc: '动植物园'
+      }, {
+        id: '0006',
+        imgUrl:
+          'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '珠江夜游'
+      }, {
+        id: '0007',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/c4c5c620f20535ff358788a508c20921.png',
+        desc: '双菊花'
+      }, {
+        id: '0008',
+        imgUrl:
+          'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/557ba25df01d7dbf1419b095a11d1319.png',
+        desc: '主题乐园'
+      }, {
+        id: '0009',
+        imgUrl:
+          'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '泡温泉'
+      }]
+    }
+  },
+  computed: {
+    // 对数据进行分页
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        // 下标除以单页总数，对数值进行向下取整
+        const page = Math.floor(index / 8)
+        // 如果不存在则创建一个空数组，用于存放数据
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        // 将item添加到对应页数的数组中
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-.icons
-  overflow: hidden
+@import '~styles/mixins.styl'
+.icons >>> .swiper-container
   height: 0
   padding-bottom: 50%
 
-  .icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
+.icon
+  position: relative
+  overflow: hidden
+  float: left
+  width: 25%
+  height: 0
+  padding-bottom: 25%
 
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      box-sizing: border-box
-      padding: .1rem
+  .icon-img
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: .44rem
+    box-sizing: border-box
+    padding: .1rem
 
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
+    .icon-img-content
+      display: block
+      margin: 0 auto
+      height: 100%
 
-    .icon-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $darkTextColor
+  .icon-desc
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    height: .44rem
+    line-height: .44rem
+    text-align: center
+    color: $darkTextColor
+    ellipsis()
 </style>

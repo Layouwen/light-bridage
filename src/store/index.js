15fc39b284251cd973656b3b671272b29a2aa3d3
liangyouwen <layouwen@gmail.com>
2020-09-28 04:20:28 +0800

    提取vuex选项单独创建文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from './state'
+import actions from './actions'
+import mutations from './mutations'
+
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    city: '北京'
-  },
-  actions: {
-    changeCity (ctx, city) {
-      ctx.commit('changeCity', city)
-    }
-  },
-  mutations: {
-    changeCity (state, city) {
-      state.city = city
-    }
-  }
-})
+export default new Vuex.Store({ state, actions, mutations })

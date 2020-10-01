d41dfe7b8fd00f023e94545b63350cf21145104c
liangyouwen <layouwen@gmail.com>
2020-10-01 15:19:39 +0800

    完成详情页头部渐隐渐现的效果

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 
 import DetailBanner from 'components/Detail/Banner'
+import DetailHeader from 'components/Detail/Header'
 
 export default {
   name: 'Detail',
-  components: { DetailBanner }
+  components: { DetailHeader, DetailBanner }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content
+  height: 50rem
 </style>

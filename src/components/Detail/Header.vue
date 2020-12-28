1223e95801f23df4f74483dd70b30e0e40250595
layouwen <layouwen@gmail.com>
2020-12-28 17:26:27 +0800

    添加详情页的Header的注释

--- src/components/Detail/Header.vue
+++ src/components/Detail/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <!-- 距离顶部小于 60 时显示的返回箭头 -->
     <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
       <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
+    <!-- 距离顶部大于 60 的时候显示的 header -->
     <div class="header-fixed" :style="opacityStyle" v-show="!showAbs">
       景点详情
       <router-link to="/">
@@ -17,7 +19,9 @@ export default {
   name: 'DetailHeader',
   data () {
     return {
+      // 默认显示小于 60 的箭头
       showAbs: true,
+      // header 的透明度
       opacityStyle: {
         opacity: 0
       }
@@ -25,21 +29,30 @@ export default {
   },
   methods: {
     handleScroll () {
+      // 获取当前具体顶部的距离
       const top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
+      // 当大于 60 的时候开始显示 header
       if (top > 60) {
+        // 计算透明度
         let opacity = top / 140
+        // 设置最大透明度为 1
         opacity = opacity > 1 ? 1 : opacity
+        // 赋值给变量
         this.opacityStyle = { opacity }
+        // 只要大于 60 则显示 header
         this.showAbs = false
       } else {
+        // 如果小于 60 则显示单箭头
         this.showAbs = true
       }
     }
   },
   mounted () {
+    // 监听滑动事件，计算header的透明度
     window.addEventListener('scroll', this.handleScroll)
   },
   destroyed () {
+    // 消除滑动事件的监听
     window.removeEventListener('scroll', this.handleScroll)
   }
 }
